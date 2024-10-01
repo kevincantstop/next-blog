d7fbab268fa7886803829497c81e0ce9341c1765
kevin <kevincantstop@gmail.com>
2024-10-01 09:21:48 +0800

    refactor code

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
     return (
-        <footer className="flex flex-col items-center bg-gray-100 py-[2rem] shadow-md">
-            <ul className="flex gap-5 my-6 text-gray-400 font-light text-[0.9rem] font-[family-name:var(--font-merriweather)]">
+        <footer className="flex flex-col items-center bg-gray-100 py-[2rem] shadow-md font-[family-name:var(--font-merriweather)]">
+            <ul className="flex gap-5 my-6 text-gray-400 font-light text-[0.9rem]">
                 <li><a href="#" className="hover:text-gray-700 hover:underline">About</a></li>
                 <li><a href="#" className="hover:text-gray-700 hover:underline">Privacy Policy</a></li>
                 <li><a href="#" className="hover:text-gray-700 hover:underline">Contact</a></li>
             </ul>
-            <p className="font-[family-name:var(--font-merriweather)] text-sm text-[#000638]">Copyright © 2024 Food Ninja Blog. All Rights Reserved.</p>
+            <p className="text-sm text-[#000638]">Copyright © {new Date().getFullYear()} Food Ninja Blog. All Rights Reserved.</p>
         </footer>
     )
 }
